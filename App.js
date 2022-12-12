97b715db2345434e3a01d8c55c06b4e4879df581
OlgaMykhailova <rosebud_z@yahoo.com>
2022-12-12 23:13:29 +0100

    added login

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StyleSheet } from "react-native";
 import { RegistrationScreen } from "./Screens/RegistrationScreen";
+import {LoginScreen} from "./Screens/LoginScreen";
 
 export default function App() {
-  return (<RegistrationScreen />);
+  return (<LoginScreen />);
 }
 
 
